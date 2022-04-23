522e6677ed2a569718dd8d178321c246d6cda3b6
Jon Winsley <jon.winsley@gmail.com>
2022-04-23 19:38:15 +0000

    Adding GitHub metadata

--- site/.eleventy.js
+++ site/.eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/_assets");
+    eleventyConfig.addPassthroughCopy({ "src/_github": "/" });
     eleventyConfig.addPassthroughCopy({ "node_modules/@fortawesome/fontawesome-free/css/all.min.css": "_assets/css/fontawesome.css" });
     eleventyConfig.addPassthroughCopy({ "node_modules/@fortawesome/fontawesome-free/webfonts": "_assets/webfonts" });
     eleventyConfig.setUseGitIgnore(false);
@@ -9,7 +10,6 @@ module.exports = function (eleventyConfig) {
         dir: {
             input: 'src',
             output: '../dist'
-        },
-        pathPrefix: '/william-perkins/'
+        }
     }
 };
